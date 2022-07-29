3f559421033b1273c322f3e1d4307549e068b2c4
soyoung2022 <yuns8671@gmail.com>
2022-07-30 01:41:15 +0900

    [DEL] unnecessary import package code

--- src/main/java/com/codeIT/moonCalendar/config/auth/SecurityConfig.java
+++ src/main/java/com/codeIT/moonCalendar/config/auth/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.codeIT.moonCalendar.config.auth;
 
 import com.codeIT.moonCalendar.domain.user.Role;
-import com.codeIT.moonCalendar.service.CustomUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
