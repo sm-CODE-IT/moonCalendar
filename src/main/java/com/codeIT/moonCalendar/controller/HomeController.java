9b8c5ff6e98e023a128f762311a8757e98c39ecd
soyoung2022 <yuns8671@gmail.com>
2022-07-30 01:49:33 +0900

    [ADD] set user's email as model attribute
    일반 회원가입 후 로그인한 회원의 이메일을 모델 객체에 담는 코드 추가

--- src/main/java/com/codeIT/moonCalendar/controller/HomeController.java
+++ src/main/java/com/codeIT/moonCalendar/controller/HomeController.java
@@ -2,7 +2,9 @@ package com.codeIT.moonCalendar.controller;
 
 import com.codeIT.moonCalendar.config.auth.LoginUser;
 import com.codeIT.moonCalendar.config.auth.dto.SessionUser;
+import com.codeIT.moonCalendar.domain.user.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,12 +14,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping("/")
-    public String index(Model model, @LoginUser SessionUser user) {
+    public String index(Model model, @LoginUser SessionUser user, @AuthenticationPrincipal CustomUserDetails customUserDetails) {
         if(user != null) {
-            model.addAttribute("userName", user.getName());
+            model.addAttribute("userName", user.getEmail());
         }
-
-        return "/user/signin";
+        if(customUserDetails != null) {
+            model.addAttribute("userName", customUserDetails.getUsername());
+        }
+        return "index";
     }
 
 }
