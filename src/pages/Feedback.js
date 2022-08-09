3eaca4f62af78ef6bcddb237afcf78f235906d98
sooking87 <sksohn01@naver.com>
2022-08-09 23:03:57 +0900

    temp Func in React

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -4,14 +4,62 @@ import MyButton from "../components/MyButton";
 import Line from "../components/Line";
 import MyFooter from "../components/MyFooter";
 
-import { useState } from "react";
-import { Navigate } from "react-router-dom";
+/* util */
+import useTheme from "../util/useTheme";
+
+import { useState, useRef } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Feedback = () => {
   /* for Plus button color */
-  const [themeMode, setThemeMode] = useState(localStorage.getItem("theme"));
-  console.log(themeMode);
+  const [themeMode, toggleTheme] = useTheme();
+  // console.log(themeMode);
   const plusButtonSrc = process.env.PUBLIC_URL + `/assets/${themeMode}Plus.png`;
+  // console.log(plusButtonSrc);
+
+  /* input 태그에 x 마크 넣으려고 */
+  const [isWriting, setIsWriting] = useState(false);
+  
+  /* for input one (어떤 문제에 대한 피드백?) */
+  const inputOneRef = useRef();
+  const [inputOneContent, setInputOneContent] = useState("");
+  
+  /* for input two (상세 내용) */
+  const inputTwoRef = useRef();
+  const [inputTwoContent, setInputTwoContent] = useState("");
+  
+    
+  /* for input four (이메일 주소를 입력) */
+  const inputFourRef = useRef();
+  const [inputFourContent, setInputFourContent] = useState("");
+    
+  /* for submit button */
+  const navigate = useNavigate();
+  const [isValid, setIsValid] = useState(true);
+  const handleSubmit = () => {
+    var validRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+    
+    if (inputOneContent.length < 1) {
+      inputOneRef.current.focus();
+      return;
+    }
+
+    if (inputTwoContent.length < 1) {
+      inputTwoRef.current.focus();
+      return;
+    }
+
+    if (!validRegex.test(inputFourContent)) {
+      inputFourRef.current.focus();
+      setIsValid(false);
+      return;
+    }
+
+    setIsValid(true);
+    if (window.confirm("제출하시겠습니까?")) {
+      navigate("/", {replace: true}); // 로그인 된 상태에서만 피드백을 들어갈 수 있으므로 이동 링크를 Calendar.js로 변경해야된다. 
+    }
+  }
 
   return (
     <div className="Feedback">
@@ -40,6 +88,9 @@ const Feedback = () => {
                 type="text"
                 className="input_box_one"
                 placeholder="피드백을 입력해주세요"
+                ref={inputOneRef}
+                value={inputOneContent}
+                onChange={(e) => setInputOneContent(e.target.value)}
               />
             </div>
             <div className="input_two">
@@ -48,27 +99,39 @@ const Feedback = () => {
                 type="text"
                 className="input_box_two"
                 placeholder="상세 내용을 입력해주세요"
+                ref={inputTwoRef}
+                value={inputTwoContent}
+                onChange={(e) => setInputTwoContent(e.target.value)}
               ></textarea>
             </div>
             <div className="input_three">
               <p className="body1">상세 이미지</p>
-              <div className="plus_button_wrapper">
+
+              <label for="file_input" className="plus_button_wrapper" >
                 <img src={plusButtonSrc} alt="" className="plus_button" />
-              </div>
+                
+              </label>
+              <input type="file" id="file_input" accept=".png, .jpg"/>
             </div>
+            
             <div className="input_four">
               <p className="body1">이메일 주소 *</p>
+              {isValid ? <></> : <p className="caption alert">* ex. xxxxxxxx@xxxxx.xxx</p> }
               <input
-                type="text"
+                type="email"
                 className="input_box_four"
                 placeholder="이메일 주소를 입력해주세요"
+                ref={inputFourRef}
+                value={inputFourContent}
+                onChange={(e) => setInputFourContent(e.target.value)}
               />
+              
             </div>
             <div className="myButton_wrapper">
               <MyButton
                 text="Submit"
                 type="long"
-                Func={() => alert("Submit")}
+                onClick={handleSubmit}
               ></MyButton>
             </div>
           </div>
