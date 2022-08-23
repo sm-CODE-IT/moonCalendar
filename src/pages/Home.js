d8410bad238d8d82534161e56e4c612167c5c2de
SooKyoung <sksohn01@naver.com>
2022-08-24 07:40:15 +0900

    fixing Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -43,180 +43,33 @@ const Home = () => {
       ></div>
       <section>
         <div className="main">
-          <Line weight={5} eachClassName={"home_line"}></Line>
-          <Line weight={3} eachClassName={"home_line"}></Line>
-          <p className="h3 content">
-            Lorem ipsum dolor Lorem ipsum dolor sit amet Lorem ipsum dolor Lorem
-            ipsum dolor sit amet
-          </p>
-          <p className="title1 content">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum
-            dolor sit amet consectetur adipisicing elit.
-          </p>
-          <Line weight={3} eachClassName={"home_line"}></Line>
+          <p className="h1 size_10 content">Moon Calendar</p>
+          <p className="title1 content"></p>
           <p className="body1 content">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam
-            veritatis, fugit perferendis aspernatur necessitatibus pariatur
-            quibusdam, soluta hic repudiandae, atque impedit iusto corporis
-            facilis at dolor quae voluptas non ullam!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. In, ullam
+            vel qui eius ea quo libero nesciunt id sequi. Libero debitis
+            reprehenderit ullam iste, modi natus dolorum earum error animi?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. In, ullam
+            vel qui eius ea quo libero nesciunt id sequi. Libero debitis
+            reprehenderit ullam iste, modi natus dolorum earum error animi?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. In, ullam
+            vel qui eius ea quo libero nesciunt id sequi. Libero debitis
+            reprehenderit ullam iste, modi natus dolorum earum error animi?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. In, ullam
+            vel qui eius ea quo libero nesciunt id sequi. Libero debitis
+            reprehenderit ullam iste, modi natus dolorum earum error animi?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. In, ullam
+            vel qui eius ea quo libero nesciunt id sequi. Libero debitis
+            reprehenderit ullam iste, modi natus dolorum earum error animi?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. In, ullam
+            vel qui eius ea quo libero nesciunt id sequi. Libero debitis
+            reprehenderit ullam iste, modi natus dolorum earum error animi?
           </p>
-          <Line weight={3} eachClassName={"home_line"}></Line>
-          <Line weight={5} eachClassName={"home_line"}></Line>
-          <p className="body1 content">
-            <br /> Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-            Repellat, veritatis repudiandae ipsam suscipit unde cumque at
-            provident laborum iusto. Velit eligendi fugit maxime, harum animi
-            vitae optio eos nesciunt soluta! Lorem, ipsum dolor sit amet
-            consectetur adipisicing elit. Nam veritatis, fugit perferendis
-            aspernatur necessitatibus pariatur quibusdam, soluta hic
-            repudiandae, atque impedit iusto corporis facilis at dolor quae
-            voluptas non ullam! <br /> Lorem ipsum dolor, sit amet consectetur
-            adipisicing elit. Repellat, veritatis repudiandae ipsam suscipit
-            unde cumque at provident laborum iusto. Velit eligendi fugit maxime,
-            harum animi vitae optio eos nesciunt soluta! <br /> <br />
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit, est
-            perspiciatis! Magnam, temporibus dolor. Exercitationem quaerat atque
-            maxime quidem hic ipsa vel facere, adipisci soluta blanditiis
-            delectus dolorum laudantium voluptatem? Lorem ipsum dolor, sit amet
-            consectetur adipisicing elit. Illo veritatis non eligendi veniam
-            excepturi, officiis asperiores perferendis alias, assumenda nobis ab
-            dolor esse quos! Natus quaerat nobis quae perferendis eius! <br />
-            <br /> <br />
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa
-            delectus magni, accusamus officiis praesentium doloremque voluptatum
-            sapiente perspiciatis consectetur adipisci ullam velit inventore
-            libero nisi in recusandae? A, assumenda sit. Lorem, ipsum dolor sit
-            amet consectetur adipisicing elit. Nam veritatis, fugit perferendis
-            aspernatur necessitatibus pariatur quibusdam, soluta hic
-            repudiandae, atque impedit iusto corporis facilis at dolor quae
-            voluptas non ullam! <br /> Lorem ipsum dolor, sit amet consectetur
-            adipisicing elit. Repellat, veritatis repudiandae ipsam suscipit
-            unde cumque at provident laborum iusto. Velit eligendi fugit maxime,
-            harum animi vitae optio eos nesciunt soluta! Lorem, ipsum dolor sit
-            amet consectetur adipisicing elit. Nam veritatis, fugit perferendis
-            aspernatur necessitatibus pariatur quibusdam, soluta hic
-            repudiandae, atque impedit iusto corporis facilis at dolor quae
-            voluptas non ullam! <br /> Lorem ipsum dolor, sit amet consectetur
-            adipisicing elit. Repellat, veritatis repudiandae ipsam suscipit
-            unde cumque at provident laborum iusto. Velit eligendi fugit maxime,
-            harum animi vitae optio eos nesciunt soluta! <br /> <br />
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit, est
-            perspiciatis! Magnam, temporibus dolor. Exercitationem quaerat atque
-            maxime quidem hic ipsa vel facere, adipisci soluta blanditiis
-            delectus dolorum laudantium voluptatem? Lorem ipsum dolor, sit amet
-            consectetur adipisicing elit. Illo veritatis non eligendi veniam
-            excepturi, officiis asperiores perferendis alias, assumenda nobis ab
-            dolor esse quos! Natus quaerat nobis quae perferendis eius! <br />
-            <br /> <br />
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa
-            delectus magni, accusamus officiis praesentium doloremque voluptatum
-            sapiente perspiciatis consectetur adipisci ullam velit inventore
-            libero nisi in recusandae? A, assumenda sit. Lorem, ipsum dolor sit
-            amet consectetur adipisicing elit. Nam veritatis, fugit perferendis
-            aspernatur necessitatibus pariatur quibusdam, soluta hic
-            repudiandae, atque impedit iusto corporis facilis at dolor quae
-            voluptas non ullam! <br /> Lorem ipsum dolor, sit amet consectetur
-            adipisicing elit. Repellat, veritatis repudiandae ipsam suscipit
-            unde cumque at provident laborum iusto. Velit eligendi fugit maxime,
-            harum animi vitae optio eos nesciunt soluta! Lorem, ipsum dolor sit
-            amet consectetur adipisicing elit. Nam veritatis, fugit perferendis
-            aspernatur necessitatibus pariatur quibusdam, soluta hic
-            repudiandae, atque impedit iusto corporis facilis at dolor quae
-            voluptas non ullam! <br /> Lorem ipsum dolor, sit amet consectetur
-            adipisicing elit. Repellat, veritatis repudiandae ipsam suscipit
-            unde cumque at provident laborum iusto. Velit eligendi fugit maxime,
-            harum animi vitae optio eos nesciunt soluta! <br /> <br />
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit, est
-            perspiciatis! Magnam, temporibus dolor. Exercitationem quaerat atque
-            maxime quidem hic ipsa vel facere, adipisci soluta blanditiis
-            delectus dolorum laudantium voluptatem? Lorem ipsum dolor, sit amet
-            consectetur adipisicing elit. Illo veritatis non eligendi veniam
-            excepturi, officiis asperiores perferendis alias, assumenda nobis ab
-            dolor esse quos! Natus quaerat nobis quae perferendis eius! <br />
-            <br /> <br />
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa
-            delectus magni, accusamus officiis praesentium doloremque voluptatum
-            sapiente perspiciatis consectetur adipisci ullam velit inventore
-            libero nisi in recusandae? A, assumenda sit. Lorem, ipsum dolor sit
-            amet consectetur adipisicing elit. Nam veritatis, fugit perferendis
-            aspernatur necessitatibus pariatur quibusdam, soluta hic
-            repudiandae, atque impedit iusto corporis facilis at dolor quae
-            voluptas non ullam! <br /> Lorem ipsum dolor, sit amet consectetur
-            adipisicing elit. Repellat, veritatis repudiandae ipsam suscipit
-            unde cumque at provident laborum iusto. Velit eligendi fugit maxime,
-            harum animi vitae optio eos nesciunt soluta! Lorem, ipsum dolor sit
-            amet consectetur adipisicing elit. Nam veritatis, fugit perferendis
-            aspernatur necessitatibus pariatur quibusdam, soluta hic
-            repudiandae, atque impedit iusto corporis facilis at dolor quae
-            voluptas non ullam! <br /> Lorem ipsum dolor, sit amet consectetur
-            adipisicing elit. Repellat, veritatis repudiandae ipsam suscipit
-            unde cumque at provident laborum iusto. Velit eligendi fugit maxime,
-            harum animi vitae optio eos nesciunt soluta! <br /> <br />
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit, est
-            perspiciatis! Magnam, temporibus dolor. Exercitationem quaerat atque
-            maxime quidem hic ipsa vel facere, adipisci soluta blanditiis
-            delectus dolorum laudantium voluptatem? Lorem ipsum dolor, sit amet
-            consectetur adipisicing elit. Illo veritatis non eligendi veniam
-            excepturi, officiis asperiores perferendis alias, assumenda nobis ab
-            dolor esse quos! Natus quaerat nobis quae perferendis eius! <br />
-            <br /> <br />
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa
-            delectus magni, accusamus officiis praesentium doloremque voluptatum
-            sapiente perspiciatis consectetur adipisci ullam velit inventore
-            libero nisi in recusandae? A, assumenda sit. Lorem, ipsum dolor sit
-            amet consectetur adipisicing elit. Nam veritatis, fugit perferendis
-            aspernatur necessitatibus pariatur quibusdam, soluta hic
-            repudiandae, atque impedit iusto corporis facilis at dolor quae
-            voluptas non ullam! <br /> Lorem ipsum dolor, sit amet consectetur
-            adipisicing elit. Repellat, veritatis repudiandae ipsam suscipit
-            unde cumque at provident laborum iusto. Velit eligendi fugit maxime,
-            harum animi vitae optio eos nesciunt soluta! Lorem, ipsum dolor sit
-            amet consectetur adipisicing elit. Nam veritatis, fugit perferendis
-            aspernatur necessitatibus pariatur quibusdam, soluta hic
-            repudiandae, atque impedit iusto corporis facilis at dolor quae
-            voluptas non ullam! <br /> Lorem ipsum dolor, sit amet consectetur
-            adipisicing elit. Repellat, veritatis repudiandae ipsam suscipit
-            unde cumque at provident laborum iusto. Velit eligendi fugit maxime,
-            harum animi vitae optio eos nesciunt soluta! <br /> <br />
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit, est
-            perspiciatis! Magnam, temporibus dolor. Exercitationem quaerat atque
-            maxime quidem hic ipsa vel facere, adipisci soluta blanditiis
-            delectus dolorum laudantium voluptatem? Lorem ipsum dolor, sit amet
-            consectetur adipisicing elit. Illo veritatis non eligendi veniam
-            excepturi, officiis asperiores perferendis alias, assumenda nobis ab
-            dolor esse quos! Natus quaerat nobis quae perferendis eius! <br />
-            <br /> <br />
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa
-            delectus magni, accusamus officiis praesentium doloremque voluptatum
-            sapiente perspiciatis consectetur adipisci ullam velit inventore
-            libero nisi in recusandae? A, assumenda sit. Lorem, ipsum dolor sit
-            amet consectetur adipisicing elit. Nam veritatis, fugit perferendis
-            aspernatur necessitatibus pariatur quibusdam, soluta hic
-            repudiandae, atque impedit iusto corporis facilis at dolor quae
-            voluptas non ullam! <br /> Lorem ipsum dolor, sit amet consectetur
-            adipisicing elit. Repellat, veritatis repudiandae ipsam suscipit
-            unde cumque at provident laborum iusto. Velit eligendi fugit maxime,
-            harum animi vitae optio eos nesciunt soluta! Lorem, ipsum dolor sit
-            amet consectetur adipisicing elit. Nam veritatis, fugit perferendis
-            aspernatur necessitatibus pariatur quibusdam, soluta hic
-            repudiandae, atque impedit iusto corporis facilis at dolor quae
-            voluptas non ullam! <br /> Lorem ipsum dolor, sit amet consectetur
-            adipisicing elit. Repellat, veritatis repudiandae ipsam suscipit
-            unde cumque at provident laborum iusto. Velit eligendi fugit maxime,
-            harum animi vitae optio eos nesciunt soluta! <br /> <br />
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit, est
-            perspiciatis! Magnam, temporibus dolor. Exercitationem quaerat atque
-            maxime quidem hic ipsa vel facere, adipisci soluta blanditiis
-            delectus dolorum laudantium voluptatem? Lorem ipsum dolor, sit amet
-            consectetur adipisicing elit. Illo veritatis non eligendi veniam
-            excepturi, officiis asperiores perferendis alias, assumenda nobis ab
-            dolor esse quos! Natus quaerat nobis quae perferendis eius! <br />
-            <br /> <br />
-          </p>
+          <p className="body1 content"></p>
+          <div className="temp"></div>
         </div>
       </section>
-
-      <MyFooter></MyFooter>
+      {/* <MyFooter></MyFooter> */}
     </div>
   );
 };
