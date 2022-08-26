6ffd5bce23670525a46824e0ba3bf71e8a297152
heeemin <101591389+heeemin@users.noreply.github.com>
2022-08-26 15:30:00 +0900

    Update App.js
    fix[1]

--- src/App.js
+++ src/App.js
@@ -9,6 +9,12 @@ import Calendar from "./pages/Calendar";
 import FinDiary from "./pages/FinDiary";
 import EditDiary from "./pages/EditDiary";
 import List from "./pages/List";
+import Signin from './pages/Signin'
+import Signin_3 from './pages/Signin_3'
+import SignupGroup from './pages/SignupGroup'
+import Signup from './pages/Signup'
+import Signup_3 from './pages/Signup_3'
+import SigninGroup from './pages/SigninGroup'
 import React, { useReducer, useEffect, useState } from "react";
 
 const reducer = (state, action) => {
@@ -123,6 +129,11 @@ const App = () => {
                   element={<EditDiary></EditDiary>}
                 ></Route>
                 <Route path="/calendar" element={<Calendar></Calendar>}></Route>
+                <Route path="/signin" element={<Signin />} />
+                <Route path="/signingroup" element={<SigninGroup />} />
+                <Route path="/signin_3" element={<Signin_3 />} />
+                <Route path="/signupgroup" element={<SignupGroup />} />
+                <Route path="/signup_3" element={<Signup_3 />} />
               </Routes>
             </div>
           </BrowserRouter>
