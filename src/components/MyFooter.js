b43926c032979316475e1028ba4de8fa9a1af08c
heeemin <101591389+heeemin@users.noreply.github.com>
2022-08-26 15:06:36 +0900

    Update MyFooter.js
    footer

--- src/components/MyFooter.js
+++ src/components/MyFooter.js
@@ -1,16 +1,38 @@
 const MyFooter = () => {
+  rimport lottie from 'lottie-web'
+import { useState, useEffect, useRef } from 'react'
+
+import './../css/MyFooter.css'
+
+const MyFooter = () => {
+  const container2 = useRef()
+  useEffect(() => {
+    lottie.loadAnimation({
+      container: container2.current,
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      animationData: require('./../pages/moon_city.json'),
+    })
+  }, [])
+
   return (
     <div className="MyFooter">
-      <br />
-      <p className="h1">나중에 footer 형식 정해야됨</p>
-      <p className="body2">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi quod
-        temporibus optio. Dolorem optio perferendis animi eligendi, dolores
-        nostrum. Dolores ab earum ratione, reiciendis tenetur molestiae
-        veritatis rerum modi perferendis?
-      </p>
+      <div className="Footer">
+        <section>
+          <br />
+          <p className="title1">Need Help? Write Feedback</p>
+          <img
+            src={process.env.PUBLIC_URL + '/assets/githubs.png'}
+            className="github"
+          />
+          <p className="title1">sm-CODE-IT/moonCalendar</p>
+          <p className="title1">김민희 | 김정윤 | 손수경 | 윤소영</p>
+        </section>
+        <div className="lottie-bg" ref={container2}></div>
+      </div>
     </div>
-  );
-};
+  )
+}
 
-export default MyFooter;
+export default MyFooter
